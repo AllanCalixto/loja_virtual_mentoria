f8ee65841b378f72c0387c3094b9d2c227c10a97
Allan <allancalixto1@gmail.com>
2024-11-30 15:21:50 -0300

    feature: relacionamento das classes Usuario e Acessos

---
+++ src/main/java/jdev/mentoria/lojavirtual/enums/TipoEndereco.java
@@ -0,0 +1,19 @@
+package jdev.mentoria.lojavirtual.enums;
+
+public enum TipoEndereco {
+    COBRANCA("Cobrança"),
+    ENTREGA("Entrega");
+
+    private String descricao;
+
+    private TipoEndereco(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public String toString() {
+        return this.descricao;
+    }
+}
--- src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
@@ -1,5 +1,7 @@
 package jdev.mentoria.lojavirtual.model;
 
+import jdev.mentoria.lojavirtual.enums.TipoEndereco;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
@@ -23,6 +25,17 @@ public class Endereco implements Serializable {
     @ManyToOne(targetEntity = Pessoa.class)
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
     private Pessoa pessoa;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEndereco tipoEndereco;
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
 
     public Long getId() {
         return id;
