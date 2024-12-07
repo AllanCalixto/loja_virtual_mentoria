21ac4cee3fc0f99f2938779afa9b797eb2701f48
Allan <allancalixto1@gmail.com>
2024-12-07 10:06:18 -0300

    feature: CupDesconto e Produto.

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/CupDesconto.java
@@ -0,0 +1,82 @@
+package jdev.mentoria.lojavirtual.model;
+
+import jdev.mentoria.lojavirtual.enums.TipoEndereco;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cup_desconto")
+@SequenceGenerator(name = "seq_cup_desconto", sequenceName = "seq_cup_desconto", allocationSize = 1, initialValue = 1)
+public class CupDesconto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cup_desconto")
+    private Long id;
+
+    private String codDesc;
+
+    private BigDecimal valorRealDesc;
+
+    private BigDecimal valorPorcentDesc;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataValidadeCupom;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodDesc() {
+        return codDesc;
+    }
+
+    public void setCodDesc(String codDesc) {
+        this.codDesc = codDesc;
+    }
+
+    public BigDecimal getValorRealDesc() {
+        return valorRealDesc;
+    }
+
+    public void setValorRealDesc(BigDecimal valorRealDesc) {
+        this.valorRealDesc = valorRealDesc;
+    }
+
+    public BigDecimal getValorPorcentDesc() {
+        return valorPorcentDesc;
+    }
+
+    public void setValorPorcentDesc(BigDecimal valorPorcentDesc) {
+        this.valorPorcentDesc = valorPorcentDesc;
+    }
+
+    public Date getDataValidadeCupom() {
+        return dataValidadeCupom;
+    }
+
+    public void setDataValidadeCupom(Date dataValidadeCupom) {
+        this.dataValidadeCupom = dataValidadeCupom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CupDesconto that = (CupDesconto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
