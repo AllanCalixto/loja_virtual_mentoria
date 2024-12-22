3c326a72ca32399fcfe7e75ac65206f547bfa6af
Allan <allancalixto1@gmail.com>
2024-12-22 18:00:02 -0300

    feature: ItemVendaLoja, NotaFiscalVenda, StatusRastreio, VendaCompraLojaVirtual.java

--- src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalVenda.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalVenda.java
@@ -28,6 +28,10 @@ public class NotaFiscalVenda implements Serializable {
     @Column(columnDefinition = "text")
     private String pdf;
 
+    @OneToOne
+    @JoinColumn(name = "venda_compra_loja_virt_id",  nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_loja_virt_fk"))
+    private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+
     public Long getId() {
         return id;
     }
@@ -74,6 +78,14 @@ public class NotaFiscalVenda implements Serializable {
 
     public void setPdf(String pdf) {
         this.pdf = pdf;
+    }
+
+    public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+        return vendaCompraLojaVirtual;
+    }
+
+    public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+        this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
     }
 
     @Override
--- src/main/java/jdev/mentoria/lojavirtual/model/StatusRastreio.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/StatusRastreio.java
@@ -20,6 +20,10 @@ public class StatusRastreio implements Serializable {
 
     private String status;
 
+    @ManyToOne
+    @JoinColumn(name = "venda_compra_loja_virt_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_compra_loja_virt_fk"))
+    private VendaCompraLojaVirtual vendaCompraLojaVirtual;
+
     public Long getId() {
         return id;
     }
@@ -50,6 +54,14 @@ public class StatusRastreio implements Serializable {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public VendaCompraLojaVirtual getVendaCompraLojaVirtual() {
+        return vendaCompraLojaVirtual;
+    }
+
+    public void setVendaCompraLojaVirtual(VendaCompraLojaVirtual vendaCompraLojaVirtual) {
+        this.vendaCompraLojaVirtual = vendaCompraLojaVirtual;
     }
 
     @Override
