aa782a2f52165e67e5c78e069a176624b56b052a
Allan <allancalixto1@gmail.com>
2024-12-22 19:41:00 -0300

    feature: AvaliacaoProduto

--- src/main/java/jdev/mentoria/lojavirtual/model/AvaliacaoProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/AvaliacaoProduto.java
@@ -2,6 +2,7 @@ package jdev.mentoria.lojavirtual.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "avaliacao_produto")
@@ -22,4 +23,48 @@ public class AvaliacaoProduto implements Serializable {
     @ManyToOne
     @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
     private Produto produto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNota() {
+        return nota;
+    }
+
+    public void setNota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AvaliacaoProduto that = (AvaliacaoProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
