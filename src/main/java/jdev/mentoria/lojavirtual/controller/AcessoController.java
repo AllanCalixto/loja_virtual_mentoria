dde584805a1d0fa0e219041f98dad8063d152213
allancalixto <allancalixto1@gmail.com>
2025-04-02 18:49:42 -0300

    feature: criação dos testes unitarios

--- src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
@@ -1,19 +1,35 @@
 package jdev.mentoria.lojavirtual.controller;
 
 import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
 import jdev.mentoria.lojavirtual.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AcessoController {
+
+    private final AcessoService acessoService;
+    private final AcessoRepository acessoRepository;
     
-    @Autowired
-    private AcessoService acessoService;
+    public AcessoController(AcessoService acessoService, AcessoRepository acessoRepository) {
+        this.acessoService = acessoService;
+        this.acessoRepository = acessoRepository;
+    }
     
-    @PostMapping("/salvarAcesso")
-    public Acesso salvarAcesso(Acesso acesso) {
-        return acessoService.save(acesso);
+    @ResponseBody
+    @PostMapping(value = "**/salvarAcesso")
+    public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) {
+        Acesso acessoSalvo = acessoService.save(acesso);
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+    }
+    
+    @ResponseBody
+    @DeleteMapping(value = "**/deleteAcesso")
+    public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) {
+        acessoRepository.deleteById(acesso.getId());
+        return new ResponseEntity("Acesso removido!", HttpStatus.OK);
     }
 }
--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTest.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTest.java
@@ -3,25 +3,50 @@ package jdev.mentoria.lojavirtual;
 import jdev.mentoria.lojavirtual.controller.AcessoController;
 import jdev.mentoria.lojavirtual.model.Acesso;
 import jdev.mentoria.lojavirtual.repository.AcessoRepository;
-import jdev.mentoria.lojavirtual.service.AcessoService;
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
-class LojaVirtualMentoriaApplicationTest {
+public class LojaVirtualMentoriaApplicationTest extends TestCase {
     
     @Autowired
     private AcessoController acessoController;
+    
+    @Autowired
+    private AcessoRepository acessoRepository;
     
     
     @Test
     public void testCadastraAcesso() {
         Acesso acesso = new Acesso();
         acesso.setDescricao("ROLE_ADMIN");
-        acessoController.salvarAcesso(acesso);
+        acesso = acessoController.salvarAcesso(acesso).getBody();
+        assertEquals("ROLE_ADMIN", acesso.getDescricao());
+        assertTrue(acesso.getId() > 0);
+        
+        Optional<Acesso> acesso2 = acessoRepository.findById(acesso.getId());
+        assertTrue(acesso2.isPresent());
+        assertEquals(acesso.getId(), acesso2.get().getId());
+        
+        acessoRepository.deleteById(acesso2.get().getId());
+        acessoRepository.flush();
+        Acesso acesso3 = acessoRepository.findById(acesso2.get().getId()).orElse(null);
+        assertEquals(true, acesso3 == null);
+        
+        /* Teste de Query */
+        acesso = new Acesso();
+        acesso.setDescricao("ROLE_ALUNO");
+        acesso = acessoController.salvarAcesso(acesso).getBody();
+        List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+        assertEquals(1, acessos.size());
+        acessoRepository.deleteById(acesso.getId());
+        
+        
     }
     
 }
